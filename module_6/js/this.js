7949564d388704b909195731f33dac443405a706
Liliia_Dudnyke <lilydalas88@gmail.com>
2025-08-12 12:09:22 +0200

    Task: software for a car (namely, pressing the dial button and reducing speed in the cruise control system).

--- module_6/js/this.js
+++ module_6/js/this.js
@@ -121,7 +121,7 @@ check(); // this = obj
 // та виводити повідомлення, чи ми рухаємось з безпечною швидкістю чи перевищуємо, функція має опрацьовувати об'єкт автомобіля як this
 
 
- 
+/*
 const SPEED = 60;
 
 const bmw = {
@@ -144,3 +144,38 @@ function speedSensor(maxSpeed) {
 
 console.log(speedSensor.call(bmw, SPEED));
 console.log(speedSensor.apply(audi, [SPEED]));
+*/
+
+
+
+
+// завдання написати програмне забезпечення для автомобіля, а саме натискання кнопки набору та зниження швидкості в системі круїз контролю.
+// Створити об'єкт cruiseControl з методами accelerate та decrease, властивостями speed та brand.
+
+ const cruiseControl = {
+     speed: 0,
+     brand: "Audi",
+     accelerate() {
+         this.speed += 10;
+         console.log(`Автомобіль ${this.brand} прискорюється, швидкість ${this.speed}`);
+     },
+     decrease() {
+         if(this.speed <= 0) {
+             console.log("Авто зупинилося");
+             return;
+         }
+
+         this.speed -= 10;
+         console.log(`Автомобіль ${this.brand} гальмує, швидкість ${this.speed}`);
+     }
+ }
+
+ cruiseControl.accelerate();
+ cruiseControl.accelerate();
+
+ cruiseControl.decrease();
+ cruiseControl.decrease();
+ cruiseControl.decrease();
+
+ console.log(cruiseControl);
+
