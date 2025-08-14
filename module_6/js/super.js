9968f03d62458cf56d37907112a7b980b5fde984
Liliia_Dudnyke <lilydalas88@gmail.com>
2025-08-14 11:43:55 +0200

    task (use super() to call the parent class constructor)

--- module_6/js/super.js
+++ module_6/js/super.js
@@ -33,3 +33,49 @@ const editor = new ContentEditor({
 });
 console.log(editor); // { #email: "mango@mail.com", posts: [] }
 console.log(editor.email); // "mango@mail.com" */
+
+//Завдання
+/*Додай класу Admin метод constructor, який приймає один параметр params- об'єкт налаштувань з двома властивостями email і access.
+  Додай класу Admin публічну властивість access, значення якої буде передаватися під час виклику конструктора.
+  Щоб показати, як буде використовуватися клас Admin, ми додали ініціалізацію екземпляра під оголошенням класу.*/
+
+ 
+  class User {
+  email;
+
+    constructor(email) {
+      this.email = email;
+    }
+  
+    get email() {
+      return this.email;
+    }
+  
+    set email(newEmail) {
+      this.email = newEmail;
+    }
+  }
+  
+  class Admin extends User {
+    static role = {
+      BASIC: "basic",
+      SUPERUSER: "superuser",
+    };
+
+  access; // публічна властивість екземпляра
+  
+    constructor(params) {
+      super(params.email);  // викликаємо конструктор батьківського класу для ініціалізації email
+      this.access = params.access; //sзберігаємо рівень доступу у публічну властивість
+  }
+}
+
+const mango = new Admin({
+  email: "mango@mail.com",
+  access: Admin.role.SUPERUSER,
+});
+
+console.log(mango.email); // "mango@mail.com"
+console.log(mango.access); // "superuser"  
+
+
