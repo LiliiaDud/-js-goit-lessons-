9cf5f08126f552b5a399ae52120b06ae0ca6ee28
Liliia_Dudnyke <lilydalas88@gmail.com>
2025-08-22 14:03:40 +0200

    task (process the form)

---
+++ module_7/js/forms.js
@@ -0,0 +1,23 @@
+/* подія submit */
+// Дії браузера за замовчуванням. 
+// Властивість elements
+// Оброби форму та збери відгук користувача в об'єкт
+
+
+const registerForm = document.querySelector(".form");
+
+registerForm.addEventListener("submit", handleSubmit);
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const form = event.target;
+  const login = form.elements.login.value;
+  const password = form.elements.password.value;
+  
+  if (login === "" || password === "") {
+    return console.log("Please fill in all the fields!");
+  }
+
+  console.log(`Login: ${login}, Password: ${password}`);
+  form.reset();
+}
